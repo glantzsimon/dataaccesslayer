d9d4e6e116804abc5e3884ee7670d319b09e590b
Simon Glantz <simon.glantz@mac.com>
2017-09-15 01:35:40 +0100

    sg - removed unused refs

--- DataAccess/Models/UserAccount.cs
+++ DataAccess/Models/UserAccount.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using Foolproof;
+using DataAnnotationsExtensions;
 using K9.Base.Globalisation;
 
 namespace K9.Base.DataAccessLayer.Models
